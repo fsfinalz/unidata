4ffd0b200bf4e4ab14e449f0d53202f003fb7d60
Yoonjaeil <fsfinalz@gmail.com>
2018-08-31 18:24:47 +0900

    category add

---
+++ app/category.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class category extends Model
+{
+    public $fillable = ['title','parent_id'];
+
+
+    /**
+     * Get the index name for the model.
+     *
+     * @return string
+    */
+    public function childs() {
+        return $this->hasMany('App\Category','parent_id','id') ;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('category-tree-view',['uses'=>'CategoryController@manageCategory']);
+Route::post('add-category',['as'=>'add.category','uses'=>'CategoryController@addCategory']);
